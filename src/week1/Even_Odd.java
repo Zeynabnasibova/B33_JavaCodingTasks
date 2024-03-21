a6a557407c4395251faa2fbdb1e375e1913e2083
Zeynab <zeynabnasibova7@gmail.com>
2024-03-21 12:16:08 -0400

    Week 1 tasks are done

---
+++ src/week1/Even_Odd.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Even_Odd {
+
+    /*
+Write a method which can identifies given number is even or odd.
+ Ex:
+     idenitify(5) -> "Odd"
+     idenitify(6) -> "Even"
+ */
+
+
+
+}
