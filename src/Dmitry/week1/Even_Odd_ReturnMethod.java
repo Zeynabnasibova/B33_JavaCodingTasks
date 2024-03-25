d8eb1195a403bd814a3619a41332451ee30d973f
TDIM <dmitry.tus@gmail.com>
2024-03-25 10:38:59 -0400

    Additional solution for Even_Odd method

---
+++ src/Dmitry/week1/Even_Odd_ReturnMethod.java
@@ -0,0 +1,35 @@
+package Dmitry.week1;
+
+public class Even_Odd_ReturnMethod {
+
+    // The solution contains a Method to Return the result:
+
+    public static void main(String[] args) {
+
+        int number = 21;
+
+        String result = checkIfEven2(number);
+        System.out.println(result) ;
+    }
+
+
+    public static String checkIfEven2(int num){
+
+       String result;
+
+        if (num %2 == 0){
+            result = "Number " + num + " is even";
+        }else{
+            result = "Number " + num + " is odd";
+        }
+        return result;
+    }
+}
+
+/*
+Write a method which can identifies given number is even or odd.
+Ex:
+     idenitify(5) -> "Odd"
+     idenitify(6) -> "Even"
+*/
+
