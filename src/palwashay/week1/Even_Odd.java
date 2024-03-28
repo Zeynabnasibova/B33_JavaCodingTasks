cad60cbcc4640666d33d8b3dd4831403cc92aa6a
Palwashay <p_zafar@yahoo.com>
2024-03-28 13:45:39 -0400

    tasks done

--- src/palwashay/week1/Even_Odd.java
+++ src/palwashay/week1/Even_Odd.java
@@ -9,6 +9,30 @@ Write a method which can identifies given number is even or odd.
      idenitify(6) -> "Even"
  */
 
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            evenAndOdd(13);
+
+
+        }
+        public static void evenAndOdd(int input) {
+            int result = 0;
+            if (input % 2 == 0) {
+                System.out.println(input + " is even number.");
+            } else {
+                System.out.println(input + " is odd number.");
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
 
 
 }
--- src/palwashay/week1/Finra.java
+++ src/palwashay/week1/Finra.java
@@ -3,9 +3,24 @@ package Dmitry.week1;
 public class Finra {
 
     public static void main(String[] args) {
+        for (int i = 1; i < 31; i++) {
+            //multi branch if statement
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
 
-
+        }
     }
+}
+
+
+
 
 
     /*
