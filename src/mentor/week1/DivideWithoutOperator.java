63865e79bb7dafe1c9a8ae157db5578b2ef8d9f1
IdeenNova <ideen@cydeo.com>
2024-03-28 12:44:49 -0400

    Week1 DivideWithoutOperator task has done

--- src/mentor/week1/DivideWithoutOperator.java
+++ src/mentor/week1/DivideWithoutOperator.java
@@ -1,11 +1,28 @@
 package mentor.week1;
 
 public class DivideWithoutOperator {
+    public static void main(String[] args) {
+        int x = 41;
+        int y = 5;
+
+        int result =0;
+
+        while ( x >= y) {
+            x = x-y;
+            result++;
+        }
+
+        System.out.println("result   = " + result);
+        System.out.println("reminder = " + x);
+
+
+    }
 
     /*
 Write a method that can divide two numbers without using division operator
 
  */
+
 
 
 }
