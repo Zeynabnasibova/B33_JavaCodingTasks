4d9b04190242b3e87a0b8c408ee1a8adb9070dc4
TDIM <dmitry.tus@gmail.com>
2024-04-04 10:35:03 -0400

    "Number_SwapTwoNumbers" - My 3 Solution of one task

---
+++ src/Dmitry/week2/Number_SwapTwoNumbers.java
@@ -0,0 +1,69 @@
+package Dmitry.week2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Number_SwapTwoNumbers {
+
+    /*
+    Swap two variables' values without using a third variable
+     */
+
+    public static void main(String[] args) {
+
+        //solution 1 -------------------------------
+
+        int a = 10;
+        int b = 20;
+
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        //solution 2 -------------------------------
+
+        int x = 10;
+        int y = 20;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(x);
+        list.add(y);
+
+        System.out.println("list1" + list);
+
+        y = list.get(0);
+        x = list.get(1);
+
+        System.out.println("x new = " + x);
+        System.out.println("y new = " + y);
+
+
+        //solution 3 -------------------------------
+
+        int k = 40;
+        int m = 60;
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(k);
+        list2.add(m);
+
+        System.out.println("list2" + list2);
+
+        for (int i = 0; i < 2; i++) {
+            if (i == 0) {
+                m = list2.get(i);
+            } else if (i == 1) {
+                k = list2.get(i);
+            }
+        }
+
+        System.out.println("k new = " + k);
+        System.out.println("m new = " + m);
+
+    }
+}
