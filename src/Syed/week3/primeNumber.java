25f16852c373952ee30d7d1e24a0966a03e61247
syedasrafi <u.nahid5@gmail.com>
2024-04-05 18:51:18 -0400

    Syeds package creation

---
+++ src/Syed/week3/primeNumber.java
@@ -0,0 +1,7 @@
+package Syed.week3;
+
+public class primeNumber {
+    /*
+        Write a method that can check if a number is prime or not
+     */
+}
---
+++ src/Syed/week3/reverseNegativeNumber.java
@@ -0,0 +1,4 @@
+package Syed.week3;
+
+public class reverseNegativeNumber {
+}
