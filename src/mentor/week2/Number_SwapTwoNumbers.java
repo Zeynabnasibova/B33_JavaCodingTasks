cb006b4aa7b563508bc56d8864161798418ef38d
IdeenNova <ideen@cydeo.com>
2024-04-11 13:45:44 -0500

    add week4 tasks

--- src/mentor/week2/Number_SwapTwoNumbers.java
+++ src/mentor/week2/Number_SwapTwoNumbers.java
@@ -7,5 +7,20 @@ public class Number_SwapTwoNumbers {
      */
 
     //solution 1
+    public static void main(String[] args) {
+        //Swap two variable' values without using a third variable
 
+
+        int a = 10;
+        int b = 20;
+        System.out.println("first a: " + a);
+        System.out.println("first b: " + b);
+        a = a + b; //a is 30 now
+        b = a - b; //b is 10 now
+        a = a - b; //a is 20 now
+        System.out.println("new a: " + a);
+        System.out.println("new b: " + b);
+
+
+    }
 }
---
+++ src/mentor/week4/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+package mentor.week4;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+    /*Write a return method that can remove the duplicated values from String
+        Ex:  removeDup("AAABBBCCC")  ==> ABC*/
+
+
+}
