6c6ce68ff7b12b655d0e989342bd1f6a3c42bd16
IdeenNova <ideen@cydeo.com>
2024-05-23 14:37:48 -0500

    add new tasks

--- src/mentor/week10/ArrayListSortAscending.java
+++ src/mentor/week10/ArrayListSortAscending.java
@@ -5,32 +5,10 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class ArrayListSortAscending {
-    public static void main(String[] args) {
-
-        ArrayList<String> list = new ArrayList<String>(Arrays.asList("Cydeo", "B25", "Java", "Selenium"));
-
-        list.add("Aaron");
-
-        sortingArrayAsc(list);
-        sortingArraysDesc(list);
-    }
-
-    public static void sortingArrayAsc(ArrayList<String> arr) {
-
-        String[] array = new String[arr.size()];
-        array = arr.toArray(array);
-        Arrays.sort(array);
-        System.out.println(Arrays.toString(array));
-    }
-
-
-    public static void sortingArraysDesc(ArrayList<String> arr) {
-        String[] array = new String[arr.size()];
-        array = arr.toArray(array);
-        Arrays.sort(array, Collections.reverseOrder());
-        System.out.println(Arrays.toString(array));
-
-    }
+/*
+Write a method that can sort the ArrayList in Ascending order without using
+the sort method.
+ */
 
 }
 
--- src/mentor/week10/MapSortByValues.java
+++ src/mentor/week10/MapSortByValues.java
@@ -6,40 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 public class MapSortByValues {
-    public static void main(String[] args) {
-        Map<String, Integer> map = new LinkedHashMap<>();
-        map.put("k0", 0);
-        map.put("k5", 5);
-        map.put("k2", 2);
-        map.put("k1", 1);
-        map.put("k3", 3);
-
-        for (String each : map.keySet()) {
-            System.out.println("key: " + each + " value: " + map.get(each));
-        }
-        System.out.println("2nd "+ map);
-        System.out.println("sortByValue(map) = " + sortByValue(map));
-
-    }
-
-
 
     /*
-        Write a method that can sort the map by values
+        Write a method that can sort the ArrayList in descending order without
+using the sort method.
      */
 
-    public static Map<String, Integer> sortByValue(Map<String, Integer> map) {
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-        list.sort(Map.Entry.comparingByValue());
 
-        map = new LinkedHashMap<>();
-
-        for (Map.Entry<String, Integer> each : list) {
-            map.put(each.getKey(), each.getValue());
-        }
-
-        return map;
-
-    }
 
 }
