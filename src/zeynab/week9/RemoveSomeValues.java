1164ac46ce68a73737d458b7d0fea210b2e437ed
Zeynab <zeynabnasibova7@gmail.com>
2024-05-30 12:32:57 -0400

    added task day 9

--- src/zeynab/week9/RemoveSomeValues.java
+++ src/zeynab/week9/RemoveSomeValues.java
@@ -2,6 +2,7 @@ package zeynab.week9;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class RemoveSomeValues {
@@ -9,15 +10,31 @@ public class RemoveSomeValues {
 //Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.
     public static void main(String[] args) {
 
-        List<Integer> number = new ArrayList<>(Arrays.asList(1, 200,2, 3,500, 4, 5, 6 ,900));
+        ArrayList<Integer> number = new ArrayList<>(Arrays.asList(1, 200,200,2, 3,500, 4, 5, 6 ,900));
+//
+//        for(int i = 0; i < number.size(); i++){
+//
+//            if(number.get(i)>100){
+//
+//                number.remove(number.get(i));
+//            }
+//        }
 
-        for(int i = 0; i < number.size(); i++){
+//        number.removeIf(n -> n > 100);
 
-            if(number.get(i)>100){
+        Iterator<Integer> iterator = number.iterator();
 
-                number.remove(number.get(i));
+        while (iterator.hasNext()) {
+
+            if (iterator.next() > 100) {
+
+                iterator.remove();
+
             }
         }
+
+
+
         System.out.println("number = " + number);
 
     }
--- src/zeynab/week9/concat2Arrays.java
+++ src/zeynab/week9/concat2Arrays.java
@@ -1,11 +1,35 @@
 package zeynab.week9;
 
+import java.util.Arrays;
+
 public class concat2Arrays {
 
 //Write a return method that can concate two arrays
-    public static void main(String[] args) {
+public static void main(String[] args) {
+
+    int[] arr1 = {1, 2, 3};
+    int[] arr2 = {4, 5, 6};
 
 
+    concat2Arrays obj = new concat2Arrays();
+    System.out.println(Arrays.toString(obj.conCat(arr1, arr2)));
+}
+
+
+
+    public int[] conCat(int[] arr1, int[] arr2) {
+
+        int[] concatArr = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            concatArr[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+
+            concatArr[arr1.length + i] = arr2[i];
+        }
+        return concatArr;
     }
-
 }
